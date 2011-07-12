d8b8c19510a8d82d08b9657636ae2524a75591f8
jonsxaero <jonsxaero@gmail.com>
2011-07-12 11:39:44 -0400

    Limpieza de modulos (cliente, detalle)

--- apps/frontend/modules/cliente/templates/_listcliente.php
+++
@@ -1,9 +0,0 @@
-<table>
-    <?php foreach ($clientes as $cliente): ?>
-    <tr class="cliente" onclick='mostrarFacturasByCliente("<?php echo $cliente->getRutCliente() ?>","#"); $(".cliente").css({"background-color" : "white", "font-weight" : "", "color" : "black"});$(this).css({"background-color" : "#6898d0", "font-weight" : "bolder", "color" : "white"});' style="cursor: pointer">
-      <td><?php echo $cliente->getRutCliente() ?></td>
-      <td><?php echo $cliente->getNombreCliente() ?></td>
-<!--      <td><?php echo $cliente->getDescripcionCliente() ?></td>-->
-    </tr>
-    <?php endforeach; ?>
-</table>
--- apps/frontend/modules/cliente/templates/newSuccess.php
+++
@@ -1,3 +0,0 @@
-<h1>New Cliente</h1>
-
-<?php include_partial('form', array('form' => $form)) ?>
